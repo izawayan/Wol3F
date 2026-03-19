4eb0671e552587cada847b8a3540cd7e6f140d02
izawayan <yaniiizawa@gmail.com>
2026-03-18 23:18:31 -0300

    chore: atualizando author e version no setup.py para nova release

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="yfinance-br-quant", # Nome que será usado no pip install
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "yfinance",
         "pandas"
     ],
-    author="Seu Nome",
+    author="Yan Izawa",
     description="Wrapper robusto do yfinance focado em tratamento de erros, delistings e DY para o mercado brasileiro.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
