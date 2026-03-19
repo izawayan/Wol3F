a2108e39dc146b3c9bce01180abd5bdd75dfabf4
Yan Izawa <134936960+izawayan@users.noreply.github.com>
2026-03-18 23:34:23 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="yfinance-br-quant", # Nome que será usado no pip install
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         "yfinance",
